8614e8424d10ac929600762e84ba97cc7017a942
pilduski <pablomuinio@hotmail.com>
2024-05-18 20:27:00 +0200

    tercera prueba

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -2,6 +2,6 @@ package paquete1;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Otro mensaje");
+        System.out.println("Vuelve a añadir otro mensaje en el método main ");
     }
 }
