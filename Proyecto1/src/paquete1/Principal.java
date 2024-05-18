3637de0dbe7d833666f027d2866d93da7642744c
pilduski <pablomuinio@hotmail.com>
2024-05-18 20:22:24 +0200

    Segunda prueba de commit

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -2,6 +2,6 @@ package paquete1;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Estamos haciendo control de versiones con Git");
+        System.out.println("Otro mensaje");
     }
 }
